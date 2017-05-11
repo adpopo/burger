4bdde0e1f4fe829755c1d36d30932ad1a07f737d
adpopo <hxuanpham@gmail.com>
2017-05-11 03:20:00 -0700

    better (super) late than never, i guess?

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+var mysql = require("mysql");
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'root',
+	password:'root',
+	database:'burgers_db'
+});
+
+// var connection = mysql.createConnection({
+// 	host:'us-cdbr-iron-east-03.cleardb.net',
+// 	user:'bf473e41a54cce',
+// 	password:'6dfce0eb',
+// 	database:'heroku_636a1695fd93986'
+// });
+
+connection.connect(function(err){
+	if(err) throw err;
+});
+
+module.exports = connection;
